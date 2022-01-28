e68e9c41d5b86933ff20d08d9a14cf4bbba0e33a
abdulhannanm <abdulhannanm85@gmail.com>
2022-01-28 11:37:34 -0800

    Home Page Done

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,38 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: "Beta Website",
+    headline: "Youth Emergency Preparedness Council",
+    description: "",
+    buttonLabel: "Learn More",
+    imgStart: '',
+    img: "images/frontimg.png",
+    alt: "Front Page"
+};
+
+
+export const homeObjTwo = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: "Be Prepared At All Times",
+    headline: "Disaster Can Strike At Any Moment",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. ",
+    imgStart: "start",
+    buttonLabel:'',
+    img: "images/ambulance-at-hospital-entrance.jpg",
+    alt: "Front Page"
+};
+
+export const homeObjThree = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: "You Can Save A Life",
+    headline: "Preparing The Youth For Dangerous Situtations",
+    buttonLabel: "Learn More",
+    imgStart: '',
+    img: "images/cpr.png",
+    alt: "Front Page"
+};
